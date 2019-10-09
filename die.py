cbc0dd3e0114c4d1c31f4b67845a5024b2f35549
Bill Hunter <billhunter@Bills-MacBook.local>
2019-10-09 17:58:21 -0500

    fixed advantage and disadvantage

--- die.py
+++ die.py
@@ -12,26 +12,24 @@ class Die(object):
     # returns the probability distribution for the greatest of 2 trials of the distribution
     def advantage(self):
         out = DiscreteDist([0.0]*len(self))
-        l = len(self)
+        l = len(self.dist)
         for i in range(l):
             for j in range(l):
                 if i >= j:
-                    out[i] += self[i]*self[j]
+                    out[i] += self.dist[i]*self.dist[j]
                 elif j > i:
-                    out[j] += self[i]*self[j]
+                    out[j] += self.dist[i]*self.dist[j]
         return out
 
     # returns the probability distribution for the lowest of 2 trials of the distribution
     def disadvantage(self):
-        out = DiscreteDist([0.0]*len(self))
-        l = len(self)
+        out = DiscreteDist([0.0]*len(self.dist))
+        l = len(self.dist)
         for i in range(l):
             for j in range(l):
                 if i >= j:
-                    out[i] += self[i]*self[j]
+                    out[i] += self.dist[i]*self.dist[j]
                 elif j > i:
-                    out[j] += self[i]*self[j]
+                    out[j] += self.dist[i]*self.dist[j]
         return out
-
-d6 = Die(6)
-print(d6)
+    
