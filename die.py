f6fa4dc3a8e420dda11e28cbe92a4c5e2fc9ae49
Bill Hunter <billhunter@Bills-MacBook.local>
2019-10-09 17:27:04 -0500

    updated DiscreteDist so that it extends the list class

--- die.py
+++ die.py
@@ -1,31 +1,7 @@
-from dist_transformer import RollDistribution
+from dist_transformer import DiscreteDist
 
 class Die(object):
 
     def __init__(self, sides):
-        self.p = RollDistribution([1/sides]*sides)
+        self.basic_dist = DiscreteDist([1/sides]*sides)
         self.n = sides
-
-    #returns a list of floats where the value at each index is the probability of the sum at that index + 1 being rolled.
-    def prob_n_dice_dist(self, rolls):
-        if rolls == 1:
-            return self.p
-        else:
-            prob_n_minus_1 = self.prob_n_dice_dist(rolls - 1)
-            for i in range(self.n):
-                prob_n_minus_1.append(0)
-            l = len(prob_n_minus_1)
-            for j in reversed(range(l)):
-                if prob_n_minus_1[j] != 0:
-                    for i in range(1,self.n+1):
-                        prob_n_minus_1[i+j] += prob_n_minus_1[j] / self.n
-                    prob_n_minus_1[j] = 0
-            return prob_n_minus_1
-    
-    #returns a list of floats where the value at each index is the probability of rolling the index + 1 after rolling 2 dice and discarding the lowest.
-    def advantage_dist(self):
-        self.p.advantage()
-
-    #returns a list of floats where the value at each index is the probability of rolling the index + 1 after rolling 2 dice and discarding the highest.
-    def disadvantage_dist(self):
-        self.p.advantage()
--- dist_transformer.py
+++ dist_transformer.py
@@ -1,58 +1,54 @@
-class DiscreteDist(object):
+# DiscreteDist is a class that extends list with changes to magic methods such that mathematical operations behave like they do on discrete probability distributions.
+# I wrote this to work on dice, so it only supports ptobability distributions with a sample space in the set of natural numbers
+class DiscreteDist(list):
 
-    def __init__(self, dist):
-        self.dist = dist
-    
-    def __len__(self):
-        return len(self.dist)
-    
     def __add__(self, other):
         total_size = len(self) + len(other)
-        out = [0] * total_size
+        out = [0.0] * total_size
         for j in range(total_size):
             for k in range(total_size):
                 try:
                     other_index = j-k-1
                     if other_index < 0:
                         continue
-                    out[j] += self.dist[k]*other.dist[other_index]
+                    out[j] += self[k]*other[other_index]
                 except:
                     pass
         return DiscreteDist(out)
-
+    
+    def __iadd__(self, other):
+        return self + other
+    
     def __mul__(self, other):
-        out = self
+        out = DiscreteDist(self)
         if other > 1:
-            for i in range(other-1):
+            for i in range(1,other):
                 out += self
         return DiscreteDist(out)
     
-    def __rmul__(self, other):
-        return self.__mul__(other)
-                
+    def __imul__(self, other):
+        return self * other
 
-    
-    def __str__(self):
-        return str(self.dist)
+    # returns the probability distribution for the greatest of 2 trials of the distribution
+    def best_of_2(self):
+        out = DiscreteDist([0.0]*len(self))
+        l = len(self)
+        for i in range(l):
+            for j in range(l):
+                if i >= j:
+                    out[i] += self[i]*self[j]
+                elif j > i:
+                    out[j] += self[i]*self[j]
+        return out
 
-
-    def advantage(self):
-        out = [0] * self.size
-        for i in range(self.size):
-            for j in range(self.size):
-                if i > j:
-                    out[i] += self.dist[i]*self.dist[j]
-                if j >= i:
-                    out[j] += self.dist[i]*self.dist[j]
-        return DiscreteDist(out)
-
-    def disadvantage(self):
-        out = [0] * self.size
-        l = len(self.dist)
-        for i in range(self.size):
-            for j in range(self.size):
-                if i < j:
-                    out[i] += self.dist[i]*self.dist[j]
-                if j <= i:
-                    out[j] += self.dist[i]*self.dist[j]
-        return DiscreteDist(out)
+    # returns the probability distribution for the lowest of 2 trials of the distribution
+    def worst_of_2(self):
+        out = DiscreteDist([0.0]*len(self))
+        l = len(self)
+        for i in range(l):
+            for j in range(l):
+                if i >= j:
+                    out[i] += self[i]*self[j]
+                elif j > i:
+                    out[j] += self[i]*self[j]
+        return out
